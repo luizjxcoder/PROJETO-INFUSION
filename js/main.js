3fe6d23b095326ab5ed7fd6a2a0a29b47f21832c
Luiz Alberto Jr <luiz.jun19@gmail.com>
2022-02-13 00:40:18 +0044

    script

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-(function () {
-   var $body = document.querySelector('body');
-   $body.classList.remove('no-js');
-   $body.classList.add('no-js');
+(function(){
+    var $body = document.querySelector('body');
+    $body.classList.remove('no-js')
+    $body.classList.add('js')
+    
+        
+    var menu = new Menu({
+        container: '.header-nav',
+        toggleBtn: '.header-btn-Menu',
+        widthEnabled: 1024 
+    })
 
-   var menu = new Menu({
-      container: '.header-nav ',
-      toggleBtn: '.header-btn-menu',
-      widthEnablade: 1024
-   })
-
-})()
+})( )
