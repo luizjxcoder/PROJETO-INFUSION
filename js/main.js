3ad6387658c5a41136ed464117878533bdcef5c6
Luiz Alberto Jr <luiz.jun19@gmail.com>
2022-02-13 19:26:01 +0044

    fim btn script

--- js/main.js
+++ js/main.js
@@ -9,5 +9,18 @@
         toggleBtn: '.header-btn-menu',
         widthEnabled: 1024 
     })
-
-})( )
+    
+    var carouselImgs = new Carousel({
+        container: '.laptop-slider .slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+    
+    var carouselQuotes = new Carousel({
+        container: '.quote-slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+})()
